c2a96d2f3dd9392a299f86425a82645ef7fe48aa
blakshmankumar <lakshmanin999@gmail.com>
2022-07-26 13:33:20 +0530

    OR operators

---
+++ oroperators.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+# purpose: Logical Operators(OR Operaters)
+# Version: 1.5
+# created date: Tuesday July 26 12:55:35 IST 2022
+# MOdified Date:
+# Author: Lakshman
+# START #  
+echo -e "Enter First Numeric value: \C"
+read -r t
+echo -e "Enter Second numeric value: \c"
+read -r b
+
+if [ $t -le 20 -o $b -ge 30 ] ; then
+echo "Statement  is true"
+else 
+echo "False, Statement Try Again."
+fi
+
+# If we provide any value in between so any one of the value will be True. foe example.
+# if we provide the value (less than 20 range) then the value will be True. (or)
+# if we provide the (second value more than 30) and the value will be True. (or)  
+# if we provide the value with in (20 in between 30) then the value will be Flase.
+
+# END #
